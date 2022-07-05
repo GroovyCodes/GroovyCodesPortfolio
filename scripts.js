3d95a4bad11b199bff422e814d00a97fdf44c466
GroovyCodes <polpages1999@outlook.com>
2022-07-05 21:23:49 +0200

    Code cleanup

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 function menuOnClick() {
-    document.getElementById("menu-bar").classList.toggle("change");
-    document.getElementById("nav").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
+    document.getElementById("burguer-menu").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("nav-toggle");
+    document.getElementById("menu-bg").classList.toggle("expand-bg");
 }
